cfbf681a4940c0e844856a7c817673113ad01472
Jason Crawford <daxapps777@gmail.com>
2017-08-26 20:05:12 -0500

    verify every 400 years is a leap year

--- reducers.js
+++ reducers.js
@@ -1,6 +1,12 @@
 module.exports.checkLeapYear = function (state = {isLeap: false}, action) {
     if (action.year % 4 === 0) {
+        if (action.year % 400 === 0) {
+            return {
+                isLeap: true
+            }
+        }
         if (action.year % 100 === 0) {
+
             return {
                 isLeap: false
             }
