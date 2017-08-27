283c3a67430200b34206ca446e646591017e96bd
Ben Pardo <benpardo@bens-mbp-4.lan>
2017-08-26 18:45:37 -0600

    testing another non-leap year

--- leap.test.js
+++ leap.test.js
@@ -10,3 +10,8 @@ test("a year that is a leap year", function () {
     store.dispatch(changeYear(2012));
     expect(store.getState().isLeap).toBe(true);
 });
+
+test("a year that is not a leap year", function () {
+    store.dispatch(changeYear(2013));
+    expect(store.getState().isLeap).toBe(false);
+});
